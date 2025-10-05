f24ec7706ccd47ca0bf37bbded3e4526466708e6
~K¹ <96512459+thebrierfox@users.noreply.github.com>
2025-10-05 15:27:39 -0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/aegisrunner/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.aegisrunner
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Aegis Runner App"
+        setContentView(textView)
+    }
+}
